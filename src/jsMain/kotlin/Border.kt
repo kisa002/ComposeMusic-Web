e9ef8c0f1c904e4c4c42d9282c771dad8312497f
kisa002 <vnycall74@naver.com>
2022-02-20 19:58:29 +0900

    [Init] load project

---
+++ src/jsMain/kotlin/Border.kt
@@ -0,0 +1,18 @@
+import org.jetbrains.compose.web.css.*
+
+fun StyleBuilder.borderTop(width: CSSNumeric, lineStyle: LineStyle, color: CSSColorValue) {
+    property("border-top", "$width $lineStyle $color")
+}
+
+fun StyleBuilder.borderRight(width: CSSNumeric, lineStyle: LineStyle, color: CSSColorValue) {
+    property("border-right", "$width $lineStyle $color")
+}
+
+fun StyleBuilder.borderBottom(width: CSSNumeric, lineStyle: LineStyle, color: CSSColorValue) {
+    property("border-bottom", "$width $lineStyle $color")
+}
+
+fun StyleBuilder.borderLeft(width: CSSNumeric, lineStyle: LineStyle, color: CSSColorValue) {
+    property("border-left", "$width $lineStyle $color")
+}
+
